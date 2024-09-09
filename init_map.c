25c2ef011873eaedc836122d4826daf80f6ea323
Wacime Elyousfi <welyousf@c1r7p8.1337.ma>
2024-09-09 05:56:13 +0100

    update

--- init_map.c
+++ init_map.c
@@ -1,47 +1,20 @@
 #include "cube.h"
 
-void	get_node_positions(t_list *map)
-{
-	int		lst_size;
-	int		count;
-
-	count = 0;
-	lst_size = ft_lstsize(map);
-	while (map)
-	{
-		if (count == 0)
-			map->position = FIRST;
-		else if (count == lst_size)
-			map->position = LAST;
-		else
-			map->position = MIDDLE;
-        count++;
-        printf("fuck %d\n", count);
-		map = map->next;
-	}
-}
-
 int create_map(void)
 {
 	char        *line = NULL;
 	int         fd = open("gnl/file", O_RDONLY);
-	t_list	    *map;
-	map = NULL;
-	line = gnl(fd);
+    char        **map;
+    char        *_map;
+
+    _map = NULL;
+    line = gnl(fd);
     while (line)
-	{
-        printf("%s", line);
-		ft_lstadd_back(&map, ft_lstnew(line));
-        printf(">>>>>>%s", (char *)ft_lstlast(map)->content);
-		line = gnl(fd);
-	}
-    map = map->next;
-    // printf("%p\n", map->next);
-    // get_node_positions(map);
-	// while (map)
-	// {
-    //     printf("%d\t%s",map->position, (char *)map->content);
-    //     map = map->next;
-	// }
-    return 0;	
+    {
+        _map = ft_strjoin(_map, line);
+        free(line);
+        line = gnl(fd);
+    }
+    map = ft_split(_map, '\n');
+    return 0;
 }
