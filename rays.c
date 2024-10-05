052c821b8761ad6c66caaa38764211ba90b25c84
Wacime Elyousfi <welyousf@c2r6p5.1337.ma>
2024-10-05 03:20:36 +0100

    update

--- rays.c
+++ rays.c
@@ -4,6 +4,7 @@ void  cast_up(t_var *var)
 {
     double *player = var->player.position;
     t_line line;
+    int flag = 0;
     line.ax = player[0];
     line.ay = player[1];
     line.by = px_to_map_grid(player[1]) * 32;
@@ -11,16 +12,19 @@ void  cast_up(t_var *var)
     while (line.bx >= 0 && line.bx <= var->mini_width
         && line.by >= 0 && line.by <= var->mini_height)
     {
-        draw_line(line, var, 0xFF0000FF);
+        flag = 1;
         line.by -= 32;
         line.bx = tan(from_deg_to_rad(var->player.angle + 30)) * (player[1] - line.by) + player[0];
     }
+    if (flag)
+        draw_line(line, var, 0xFF0000FF);
 }
 
 void  cast_down(t_var *var)
 {
     double *player = var->player.position;
     t_line line;
+    int flag = 0;
     line.ax = player[0];
     line.ay = player[1];
     line.by = px_to_map_grid(player[1]) * 32 + 32;
@@ -28,10 +32,12 @@ void  cast_down(t_var *var)
     while (line.bx >= 0 && line.bx <= var->mini_width
         && line.by >= 0 && line.by <= var->mini_height)
     {
-        draw_line(line, var, 0xFF0000FF);
+        flag = 1;
         line.by += 32;
         line.bx = tan(from_deg_to_rad(var->player.angle + 30)) * (player[1] - line.by) + player[0];
     }
+    if (flag)
+        draw_line(line, var, 0xFF0000FF);
 }
 
 void cast(t_var *var)
