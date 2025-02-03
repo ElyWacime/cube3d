4c0f769092464de97f6a21f55b24b9f6c8f59720
elywacime <yousfi.wacime@gmail.com>
2025-02-03 12:38:15 +0100

    some player movement fixes again

--- move_player.c
+++ move_player.c
@@ -4,26 +4,26 @@ int check_if_wall_up_movement(t_var *var, t_point new_position)
 {
     if (var->player.angle >= 0 && var->player.angle < 90)
     {
-        if (var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x + 0.1)] != '1'
-            && var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x + 0.1)] != 'P')
+        if (var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x + 1)] != '1'
+            && var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x + 1)] != 'P')
             return 1;
     }
     else if (var->player.angle >= 90 && var->player.angle < 180)
     {
-        if (var->map[px_to_map_grid(new_position.y - 0.1)][px_to_map_grid(new_position.x)] != '1'
-            && var->map[px_to_map_grid(new_position.y - 0.1)][px_to_map_grid(new_position.x)] != 'P')
+        if (var->map[px_to_map_grid(new_position.y - 1)][px_to_map_grid(new_position.x)] != '1'
+            && var->map[px_to_map_grid(new_position.y - 1)][px_to_map_grid(new_position.x)] != 'P')
             return 1;
     }
     else if (var->player.angle >= 180 && var->player.angle < 270)
     {
-        if (var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x - 0.1)] != '1'
-            && var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x - 0.1)] != 'P')
+        if (var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x - 1)] != '1'
+            && var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x - 1)] != 'P')
             return 1;
     }
     else if (var->player.angle >= 270 && var->player.angle < 360)
     {
-        if (var->map[px_to_map_grid(new_position.y + 0.1)][px_to_map_grid(new_position.x)] != '1'
-            && var->map[px_to_map_grid(new_position.y + 0.1)][px_to_map_grid(new_position.x)] != 'P')
+        if (var->map[px_to_map_grid(new_position.y + 1)][px_to_map_grid(new_position.x)] != '1'
+            && var->map[px_to_map_grid(new_position.y + 1)][px_to_map_grid(new_position.x)] != 'P')
             return 1;
     }
     // else
@@ -39,26 +39,26 @@ int check_if_wall_down_movement(t_var *var, t_point new_position)
 {
     if (var->player.angle >= 0 && var->player.angle < 90)
     {
-        if (var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x - 0.1)] != '1'
-            && var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x - 0.1)] != 'P')
+        if (var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x - 1)] != '1'
+            && var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x - 1)] != 'P')
             return 1;
     }
     else if (var->player.angle >= 90 && var->player.angle < 180)
     {
-        if (var->map[px_to_map_grid(new_position.y + 0.1)][px_to_map_grid(new_position.x)] != '1'
-            && var->map[px_to_map_grid(new_position.y + 0.1)][px_to_map_grid(new_position.x)] != 'P')
+        if (var->map[px_to_map_grid(new_position.y + 1)][px_to_map_grid(new_position.x)] != '1'
+            && var->map[px_to_map_grid(new_position.y + 1)][px_to_map_grid(new_position.x)] != 'P')
             return 1;
     }
     else if (var->player.angle >= 180 && var->player.angle < 270)
     {
-        if (var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x + 0.1)] != '1'
-            && var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x + 0.1)] != 'P')
+        if (var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x + 1)] != '1'
+            && var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x + 1)] != 'P')
             return 1;
     }
     else if (var->player.angle >= 270 && var->player.angle < 360)
     {
-        if (var->map[px_to_map_grid(new_position.y - 0.1)][px_to_map_grid(new_position.x)] != '1'
-            && var->map[px_to_map_grid(new_position.y - 0.1)][px_to_map_grid(new_position.x)] != 'P')
+        if (var->map[px_to_map_grid(new_position.y - 1)][px_to_map_grid(new_position.x)] != '1'
+            && var->map[px_to_map_grid(new_position.y - 1)][px_to_map_grid(new_position.x)] != 'P')
             return 1;
     }
     // else
@@ -74,26 +74,26 @@ int check_if_wall_left_movement(t_var *var, t_point new_position)
 {
     if (var->player.angle >= 0 && var->player.angle < 90)
     {
-        if (var->map[px_to_map_grid(new_position.y - 0.1)][px_to_map_grid(new_position.x)] != '1'
-            && var->map[px_to_map_grid(new_position.y - 0.1)][px_to_map_grid(new_position.x)] != 'P')
+        if (var->map[px_to_map_grid(new_position.y - 1)][px_to_map_grid(new_position.x)] != '1'
+            && var->map[px_to_map_grid(new_position.y - 1)][px_to_map_grid(new_position.x)] != 'P')
             return 1;
     }
     else if (var->player.angle >= 90 && var->player.angle < 180)
     {
-        if (var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x - 0.1)] != '1'
-            && var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x - 0.1)] != 'P')
+        if (var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x - 1)] != '1'
+            && var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x - 1)] != 'P')
             return 1;
     }
     else if (var->player.angle >= 180 && var->player.angle < 270)
     {
-        if (var->map[px_to_map_grid(new_position.y + 0.1)][px_to_map_grid(new_position.x)] != '1'
-            && var->map[px_to_map_grid(new_position.y + 0.1)][px_to_map_grid(new_position.x)] != 'P')
+        if (var->map[px_to_map_grid(new_position.y + 1)][px_to_map_grid(new_position.x)] != '1'
+            && var->map[px_to_map_grid(new_position.y + 1)][px_to_map_grid(new_position.x)] != 'P')
             return 1;
     }
     else if (var->player.angle >= 270 && var->player.angle < 360)
     {
-        if (var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x + 0.1)] != '1'
-            && var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x + 0.1)] != 'P')
+        if (var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x + 1)] != '1'
+            && var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x + 1)] != 'P')
             return 1;
     }
     // else
@@ -109,26 +109,26 @@ int check_if_wall_right_movement(t_var *var, t_point new_position)
 {
     if (var->player.angle >= 0 && var->player.angle < 90)
     {
-        if (var->map[px_to_map_grid(new_position.y + 0.1)][px_to_map_grid(new_position.x)] != '1'
-            && var->map[px_to_map_grid(new_position.y + 0.1)][px_to_map_grid(new_position.x)] != 'P')
+        if (var->map[px_to_map_grid(new_position.y + 1)][px_to_map_grid(new_position.x)] != '1'
+            && var->map[px_to_map_grid(new_position.y + 1)][px_to_map_grid(new_position.x)] != 'P')
             return 1;
     }
     else if (var->player.angle >= 90 && var->player.angle < 180)
     {
-        if (var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x + 0.1)] != '1'
-            && var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x + 0.1)] != 'P')
+        if (var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x + 1)] != '1'
+            && var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x + 1)] != 'P')
             return 1;
     }
     else if (var->player.angle >= 180 && var->player.angle < 270)
     {
-        if (var->map[px_to_map_grid(new_position.y - 0.1)][px_to_map_grid(new_position.x)] != '1'
-            && var->map[px_to_map_grid(new_position.y - 0.1)][px_to_map_grid(new_position.x)] != 'P')
+        if (var->map[px_to_map_grid(new_position.y - 1)][px_to_map_grid(new_position.x)] != '1'
+            && var->map[px_to_map_grid(new_position.y - 1)][px_to_map_grid(new_position.x)] != 'P')
             return 1;
     }
     else if (var->player.angle >= 270 && var->player.angle < 360)
     {
-        if (var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x - 0.1)] != '1'
-            && var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x - 0.1)] != 'P')
+        if (var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x - 1)] != '1'
+            && var->map[px_to_map_grid(new_position.y)][px_to_map_grid(new_position.x - 1)] != 'P')
             return 1;
     }
     // else
